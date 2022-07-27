5585479e14daa136587761d31378c994a01e932b
Vitor Guilhermo <viguilhermo@hotmail.com>
2022-07-27 18:53:11 -0300

    Componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 function App() {
     
@@ -17,7 +18,9 @@ function App() {
                 </aside>
                 
                 <main>
-                    posts
+                    <Post />
+                    <Post />
+                    <Post />
                 </main>
             </div>
         </div>
