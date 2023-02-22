17ab7ebe9a6dea91f2e39cf4ecc312f95d1db1a6
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-02-22 20:32:13 -0300

    add: funcion likes and validate form

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import styles from "./Comment.module.css";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 
 export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0)
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
+
+        // evita o problema do context do react, usa dessa forma quando a atualização de uma variável depende do valor anterior
+        // setLikeCount((state) => {
+        //     return state + 1;
+        // })
     }
 
     return (
@@ -30,9 +42,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
